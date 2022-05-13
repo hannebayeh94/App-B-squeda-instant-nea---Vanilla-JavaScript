f831ebae9529379b8bd7f001af9f471b03353b37
hannebayeh94 <56558335+hannebayeh94@users.noreply.github.com>
2022-05-13 12:28:39 -0500

    comentarios add

--- app.js
+++ app.js
@@ -1,52 +1,39 @@
 
 
-const inputBuscar = document.getElementById('buscar');
-const celdas = document.getElementsByTagName('td');
 
-// treemo los datos
+//referenciamos a los elementos del DOM
+const inputBuscar = document.getElementById('buscar')
+const celdas = document.getElementsByTagName('td')
 
-let url = 'https://jsonplaceholder.typicode.com/users'
-fetch(url)
-    .then(response => response.json())
-    .then(data => mostrarDatos(data))
-    .catch(error => console.log(error));
-
+//traemos los datos desde una API
+let uri = 'https://jsonplaceholder.typicode.com/users'
+fetch(uri)
+    .then( response => response.json() )
+    .then( json => mostrarDatos(json) )
+    .catch( e => console.log(e) )
 
 const mostrarDatos = (data) => {
-
-    console.log(data);
-
-    let body = "";
-    for (let i in data) {
-
-        body += `<tr><td>${data[i].name}</td></tr>`;
-       
+    //console.log(data)
+    let body=''
+    for(let i=0; i<data.length; i++){
+        body += `<tr><td>${data[i].name}</td></tr>`
     }
+    document.querySelector('.datos').innerHTML = body
+}    
 
-    document.querySelector('tbody').innerHTML = body;
-
-}
-
-
-// buscar datos
-inputBuscar.addEventListener('keyup', (e) => {
-    let texto = e.target.value;
-    console.log(texto);
-    let er = new RegExp(texto, 'i');
-
-    for (let i = 0; i < celdas.length; i++) {
-
+//Búsqueda
+inputBuscar.addEventListener('keyup', (e)=>{
+    let texto = e.target.value
+    //console.log(texto)
+    let er = new RegExp(texto, "i")
+    for(let i=0; i<celdas.length; i++) {
         let valor = celdas[i]
-        // console.log(valor);
-        if (er.test(valor.innerText)) {
-
-            valor.classList.remove('ocultar');
-            
-        } else {
-            console.log(valor);
-            valor.classList.add('ocultar');
-
+        //console.log(valor)
+        if(er.test(valor.innerText)){
+            valor.classList.remove("ocultar")
+        }else{
+            console.log(valor)
+            valor.classList.add("ocultar")
         }
-
     }
 })
